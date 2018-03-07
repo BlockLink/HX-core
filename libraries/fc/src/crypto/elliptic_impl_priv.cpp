76c25c6bfb5fcd3ea6d5380890d43385ad7515c0
wens.wq <wens.wq@gmail.com>
2018-03-07 17:12:10 +0800

    add review modification

--- libraries/fc/src/crypto/elliptic_impl_priv.cpp
+++ libraries/fc/src/crypto/elliptic_impl_priv.cpp
@@ -97,7 +97,7 @@ namespace fc { namespace ecc {
         unsigned int counter = 0;
         do
         {
-            FC_ASSERT( secp256k1_ecdsa_sign_compact( detail::_get_context(), (unsigned char*) digest.data(), (secp256k1_ecdsa_signature*) result.begin() + 1, (unsigned char*) my->_key.data(), NULL, &counter, &recid ));
+            FC_ASSERT( secp256k1_ecdsa_sign_compact( detail::_get_context(), (unsigned char*) digest.data(), (secp256k1_ecdsa_signature*)( result.begin() + 1 ), (unsigned char*) my->_key.data(), NULL, &counter, &recid ));
         } while( require_canonical && !public_key::is_canonical( result ) );
         result.begin()[0] = 27 + 4 + recid;
         return result;
